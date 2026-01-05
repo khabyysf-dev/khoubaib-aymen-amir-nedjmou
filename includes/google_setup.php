57974d93d08e6cba4a5ffe8056a5a9efd2489035
khabyysf-dev <khabyysf@gmail.com>
2026-01-05 18:48:46 +0100

    preparing for merge

--- includes/google_setup.php
+++ includes/google_setup.php
@@ -7,8 +7,8 @@ $client_secret = "YOUR_CLIENT_SECRET";
 $redirectUri = 'http://localhost/login/google-callback.php';
 
 $client = new Google_Client();
-$clientID = '302882991968-4efjojbdcnigsvt1qah8jk2of2nm0en8.apps.googleusercontent.com';
-$clientSecret = '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';
+$client->setClientId($clientID);
+$client->setClientSecret($clientSecret);
 $client->setRedirectUri($redirectUri);
 $client->addScope("email");
 $client->addScope("profile");
